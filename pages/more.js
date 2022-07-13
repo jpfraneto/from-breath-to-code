3e476d1a3fae94c168ca205474045beceb95f240
jpfraneto <jpfraneto@gmail.com>
2022-07-13 14:00:12 -0400

    initial deploy

---
+++ pages/more.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react';
+
+const More = () => {
+  return (
+    <div>
+      THIS PROJECT IS MY TAKE ON THE INTERSECTION BETWEEN BREATHWORK AND
+      PROGRAMMING. WELCOME!
+      <br />
+      <Link href='/'>
+        <a>Back</a>
+      </Link>
+    </div>
+  );
+};
+
+export default More;
